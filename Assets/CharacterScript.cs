e7bbd2e2c7c655efc62b18dfd3ebde0d188ea9d8
1630228 <nastyahmede@gmail.com>
2019-10-10 22:35:45 +0000

    Hi Erikiko.

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using static UnityEngine.Input;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		var horizD =
+		(GetKey(KeyCode.RightArrow) ? 1 : 0) -
+		(GetKey(KeyCode.LeftArrow) ? 1 : 0);
+		print(horizD);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(horizD, 0);
+	}
+}
