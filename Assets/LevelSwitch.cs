f17dcd050fb8239c26a844e8cae1b83c90431e08
1630228 <nastyahmede@gmail.com>
2019-11-08 23:29:56 +0000

    'Minor', insignificant changes.

---
+++ Assets/LevelSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LevelSwitch : MonoBehaviour {
+	void Start() { }
+	void Update() { }
+
+	Task<bool> SetLevelTask = null;
+	Task<bool> SetLevel(int lvl) {
+		SetLevelTask = new Task<bool>(() => {
+			return true;
+		});
+		SetLevelTask.Start();
+		return SetLevelTask;
+	}
+}
