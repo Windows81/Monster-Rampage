f996eeea38d0f0b1a8df3eb42cfa7fb8c6e17a4a
Erik_p <estevan_erik@yahoo.com>
2019-11-16 18:54:55 -0800

    Added Health counter, Game Over Sceen, and started Lvl 2 Scene
    -Packaged health and score in "stats" file
    -Will be working on lvl 2 scene, but have not done the transition yet

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -5,31 +5,37 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+
     public static float score;
+
     Text text_score;
+
 
     private void Start()
     {
-        //Declare variables
         text_score = GetComponent<Text>();
-        score = 0;
+
+        score = 0;        //text that score starts with
+
+
+
+
     }
 
     public void Update()
     {
-        //display score
-        if (score < 0)
-        {
-            score = 0;
-        }
-        text_score.text = "" + score;
-    }
 
+
+        text_score.text = "" + score;
+
+
+
+
+    }
 
     public static void Addpoints(int pointstoAdd)
 
     {
-        //add points using Addpoints function
         score += pointstoAdd;
 
 
@@ -37,3 +43,5 @@ public class ScoreManager : MonoBehaviour
 
 
 }
+
+
---
+++ Assets/livesManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class livesManager : MonoBehaviour
+{
+    public static int lives;
+    public static int livestoAdd;
+    Text text_lives;
+    public GameObject GameOverScreen;
+    public GameObject Protagonist;
+    public GameObject stats;
+ 
+
+
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        text_lives = GetComponent<Text>();
+        lives = 3;
+        GameOverScreen.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        text_lives.text = "" + lives;
+
+        if (lives <= 0)
+        {
+
+            GameOverScreen.SetActive(true);
+            Destroy(Protagonist.gameObject);
+            Destroy(stats.gameObject);
+
+
+        }
+
+    }
+
+    public static void Addpoints(int livestoAdd)
+    {
+        lives += livestoAdd;
+    }
+
+}
+
