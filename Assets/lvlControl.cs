92152a8bfcf441f11f9c3056889c2c5358c7029c
Erik_p <estevan_erik@yahoo.com>
2019-11-27 13:34:26 -0800

    Push 3: Created level changes, placed new background placeholder
    will start on level designs and damage

---
+++ Assets/lvlControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class lvlControl : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //load level
+            SceneManager.LoadScene(index);
+
+            //load with scene name
+            SceneManager.LoadScene(levelName);
+
+            
+
+        }
+      
+
+    }
+
+
+
+
+}
